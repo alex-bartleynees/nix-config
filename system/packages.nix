6ba5a0e841212c35f1755e6f779630a7db6955df
Alex Bartley Nees <alexbartleynees@gmail.com>
2024-12-28 11:04:28 +1300

    Add steam and thunar

--- system/packages.nix
+++ system/packages.nix
@@ -32,6 +32,8 @@
     pulseaudio
   ];
 
+  programs.thunar.enable = true;
+
   programs.sway = {
     enable = true;
     wrapperFeatures.gtk = true;
@@ -40,6 +42,13 @@
   programs.nm-applet = { enable = true; };
 
   programs.dconf.enable = true;
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+};
 
   qt = {
     enable = true;
