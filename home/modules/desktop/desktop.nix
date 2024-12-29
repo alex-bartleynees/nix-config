8eaf876530962545938643b2b92781e2b4b52e08
Alex Bartley Nees <alexbartleynees@gmail.com>
2024-12-30 08:25:30 +1300

    Add sway config

--- home/modules/desktop/desktop.nix
+++ home/modules/desktop/desktop.nix
@@ -1,4 +1,4 @@
-{ pkgs, inputs, background, ... }: {
+{ pkgs, inputs, background, hostName, ... }: {
   imports = [ ../vscode ];
   home.packages = with pkgs; [
     waybar
@@ -24,9 +24,39 @@
       recursive = true;
     };
 
+    ".config/sway/colorscheme".source = "${inputs.dotfiles}/configs/sway/colorscheme";
+    ".config/sway/config".source = "${inputs.dotfiles}/configs/sway/config";
+    ".config/sway/lock.sh".source = "${inputs.dotfiles}/configs/sway/lock.sh";
+
     ".config/sway/background".text = ''
       set $background ${background.wallpaper}
     '';
+
+    ".config/sway/${hostName}/config".text = ''
+      set $mainMonitor DP-4
+      set $secondaryMonitor DP-6
+      set $lock ~/.config/sway/lock.sh
+
+      exec swaymsg output $mainMonitor pos 0 0 res 2560x1440
+      exec swaymsg output $secondaryMonitor pos 2560 0 res 2560x1440 transform 90
+      exec swaymsg focus output $mainMonitor
+
+      workspace 1 output $mainMonitor
+      workspace 2 output $secondaryMonitor
+
+      exec sway-audio-idle-inhibit
+
+      # Idle configuration
+      exec swayidle \
+          timeout 300 'exec $lock' \
+          timeout 600 'swaymsg "output $mainMonitor dpms off"; swaymsg "output $secondaryMonitor dpms off"' \
+          after-resume 'swaymsg "output $mainMonitor dpms on"; swaymsg "output $secondaryMonitor dpms on"' \
+          before-sleep 'exec $lock; swaymsg "output $mainMonitor dpms off"; swaymsg "output $secondaryMonitor dpms off"'
+
+      # Wake command
+      bindsym $mod+Shift+w exec swaymsg "output $mainMonitor dpms on; output $secondaryMonitor dpms on"
+    '';
+
   };
 
   home.sessionPath = [ "$HOME/.config/rofi/scripts" ];
