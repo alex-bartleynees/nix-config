8348bcde9a7edb3eabc7fa4465bf3af6f073832a
Alex Bartley Nees <alexbartleynees@gmail.com>
2024-12-30 08:25:57 +1300

    Update pam keyring for greetd

--- system/hosts/desktop/modules/security.nix
+++ system/hosts/desktop/modules/security.nix
@@ -2,4 +2,5 @@
   security.pam.services.gdm.enableGnomeKeyring = true;
   security.pam.services.swaylock = { text = "auth include login"; };
   security.pam.services.login.enableGnomeKeyring = true;
+  security.pam.services.greetd.enableGnomeKeyring = true;
 }
