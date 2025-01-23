f6b7543e45eed23d9a832140bfda741bfba7ccd7
Alex Bartley Nees <alexbartleynees@gmail.com>
2025-01-24 09:10:48 +1300

    Remove unneeded system

--- hosts/macbook/default.nix
+++ hosts/macbook/default.nix
@@ -4,7 +4,6 @@ let
   username = "alexbartleynees";
   homeDirectory = "/Users/alexbartleynees";
 in nix-darwin.lib.darwinSystem {
-  system = "aarch64-darwin";
   modules = [ ./configuration.nix { _module.args.self = inputs.self; } ]
     ++ (import ../../shared/darwin-home-manager.nix {
       inherit inputs username homeDirectory;
