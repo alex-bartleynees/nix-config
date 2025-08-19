3f7c26a1434672176da75057b744dc2861c0eff6
Alex Bartley Nees <alexbartleynees@gmail.com>
2025-08-19 16:51:55 +1200

    Add udev rule to prevent usb mouse preventing suspend/hibernate

--- hosts/desktop/nixos/configuration.nix
+++ hosts/desktop/nixos/configuration.nix
@@ -37,6 +37,11 @@
     image = background.wallpaper;
   };
 
+  services.udev.extraRules = ''
+    # Disable wake-up for Logitech USB Receiver (C548)
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="046d", ATTRS{idProduct}=="c548", ATTR{power/wakeup}="disabled"
+  '';
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
