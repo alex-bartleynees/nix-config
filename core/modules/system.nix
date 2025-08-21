7798d59649b9d9430a0a4d4750cacb198738f35a
Alex Bartley Nees <alexbartleynees@gmail.com>
2025-08-22 07:53:11 +1200

    Use zen kernel

--- core/modules/system.nix
+++ core/modules/system.nix
@@ -35,8 +35,9 @@ in {
       # Bootloader
       boot.loader.systemd-boot.enable = true;
       boot.loader.efi.canTouchEfiVariables = true;
+      boot.loader.systemd-boot.consoleMode = "max";
 
-      boot = { kernelPackages = pkgs.linuxPackages_latest; };
+      boot = { kernelPackages = pkgs.linuxKernel.packages.linux_zen; };
 
       # Hardware
       hardware.graphics = { enable = true; };
