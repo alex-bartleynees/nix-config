82d7e1701ecffd3aae9392b088701657a0c84ac3
Alex Bartley Nees <alexbartleynees@gmail.com>
2025-08-23 15:47:04 +1200

    Fix cosmic for media server

--- hosts/media/default.nix
+++ hosts/media/default.nix
@@ -2,6 +2,10 @@
 let
   inherit (inputs) nixpkgs;
   system = builtins.currentSystem;
+  pkgs-cosmic = import inputs.cosmic-nixpkgs {
+    system = "x86_64-linux";
+    config.allowUnfree = true;
+  };
   shared = import ../../shared/nixos-default.nix {
     inherit inputs;
     theme = "tokyo-night";
@@ -9,6 +13,7 @@ let
 in nixpkgs.lib.nixosSystem {
   specialArgs = {
     inherit inputs;
+    pkgs-cosmic = pkgs-cosmic;
     background = import ../../shared/background.nix { inherit inputs; };
   };
   modules = shared.getImports {
