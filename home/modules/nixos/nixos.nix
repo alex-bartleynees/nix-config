26e81dd8464bd33003f6fa8de2f4cb9c51d66f67
Alex Bartley Nees <alexbartleynees@gmail.com>
2025-08-30 18:59:51 +1200

    remove duplicate imports

--- home/modules/nixos/nixos.nix
+++ home/modules/nixos/nixos.nix
@@ -1,13 +1,9 @@
 { pkgs, inputs, hostName, theme, lib, ... }: {
   imports = [
     ../vscode
-    ../waybar
     ../sway
     ../hyprland
-    ../rofi
-    ../dunst
     ../rider
-    ../obsidian
     ../linux
   ];
   home.packages = with pkgs; [ qbittorrent-enhanced ];
