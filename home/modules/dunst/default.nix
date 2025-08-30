08ade5b946cdccfb237b98dc76f0986dcc14082d
Alex Bartley Nees <alexbartleynees@gmail.com>
2025-08-30 18:55:01 +1200

    Update dunst

--- home/modules/dunst/default.nix
+++ home/modules/dunst/default.nix
@@ -1,7 +1,24 @@
-{ pkgs, inputs, theme, ... }: {
-  home.packages = with pkgs; [ dunst ];
-
-  home.file = {
-    ".config/dunst/dunstrc".source = "${inputs.dotfiles}/configs/dunst/dunstrc";
+{ pkgs, inputs, theme, lib, ... }: {
+  services.dunst = {
+    enable = true;
+    settings = {
+      global = {
+        frame_color = lib.mkForce theme.themeColors.active_border;
+        separator_color = lib.mkForce "frame";
+      };
+      urgency_low = {
+        background = lib.mkForce theme.themeColors.groupbar_inactive;
+        foreground = lib.mkForce theme.themeColors.text;
+      };
+      urgency_normal = {
+        background = lib.mkForce theme.themeColors.groupbar_inactive;
+        foreground = lib.mkForce theme.themeColors.text;
+      };
+      urgency_critical = {
+        background = lib.mkForce theme.themeColors.groupbar_inactive;
+        foreground = lib.mkForce theme.themeColors.text;
+        frame_color = lib.mkForce theme.themeColors.locked_active;
+      };
+    };
   };
 }
