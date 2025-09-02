655f493ac91b1349de768c7513ef753ad4df0a4e
Alex Bartley Nees <alexbartleynees@gmail.com>
2025-09-02 20:26:42 +1200

    Update snapshots

--- core/modules/snapshots.nix
+++ core/modules/snapshots.nix
@@ -5,31 +5,39 @@ in {
     lib.mkEnableOption "Enable btrfs filesystem snapshots";
   config = lib.mkIf cfg.enable {
     services.snapper = {
-      snapshotInterval = "daily";
+      snapshotInterval = "hourly";
 
       configs = {
         root = {
-          SUBVOLUME = "/@";
+          SUBVOLUME = "/";
           TIMELINE_CREATE = true;
           TIMELINE_CLEANUP = true;
-          TIMELINE_LIMIT_HOURLY = "0";
+          TIMELINE_LIMIT_HOURLY = "24";
           TIMELINE_LIMIT_DAILY = "7";
-          TIMELINE_LIMIT_WEEKLY = "0";
-          TIMELINE_LIMIT_MONTHLY = "0";
+          TIMELINE_LIMIT_WEEKLY = "4";
+          TIMELINE_LIMIT_MONTHLY = "3";
           TIMELINE_LIMIT_YEARLY = "0";
         };
 
         home = {
-          SUBVOLUME = "/@home";
+          SUBVOLUME = "/home";
           TIMELINE_CREATE = true;
           TIMELINE_CLEANUP = true;
-          TIMELINE_LIMIT_HOURLY = "0";
+          TIMELINE_LIMIT_HOURLY = "24";
           TIMELINE_LIMIT_DAILY = "7";
-          TIMELINE_LIMIT_WEEKLY = "0";
-          TIMELINE_LIMIT_MONTHLY = "0";
+          TIMELINE_LIMIT_WEEKLY = "4";
+          TIMELINE_LIMIT_MONTHLY = "3";
           TIMELINE_LIMIT_YEARLY = "0";
         };
       };
     };
+
+    system.activationScripts.snapperSubvolumes = ''
+      if [ ! -d /home/.snapshots ]; then
+        ${pkgs.btrfs-progs}/bin/btrfs subvolume create /home/.snapshots
+        chmod 755 /home/.snapshots
+        chown root:root /home/.snapshots
+      fi
+    '';
   };
 }
