4b4043f6ba26dc9ffbcaafb0a44730ae305471b0
Alex Bartley Nees <alexbartleynees@gmail.com>
2025-09-13 07:58:26 +1200

    Add kanshi service for thinkpad

--- home/modules/thinkpad/default.nix
+++ home/modules/thinkpad/default.nix
@@ -1,3 +1,30 @@
-{ pkgs, inputs, hostName, theme, lib, ... }: {
+{ pkgs, ... }: {
   imports = [ ../vscode ../rider ../linux ];
+
+  home.packages = with pkgs; [ kanshi ];
+
+  services.kanshi = {
+    enable = true;
+    systemdTarget = "river-session.target";
+
+    profiles = {
+      # ThinkPad + External LG UltraGear display
+      docked = {
+        outputs = [
+          {
+            criteria = "eDP-1"; # eDP-1 (laptop)
+            mode = "1920x1080@60";
+            position = "0,1440";
+            status = "enable";
+          }
+          {
+            criteria = "DP-1"; # DP-1 (external)
+            mode = "2560x1440@60";
+            position = "0,0";
+            status = "enable";
+          }
+        ];
+      };
+    };
+  };
 }
