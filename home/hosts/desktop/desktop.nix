a9b7392c5e98c9ec37af101626ad0f04ba9cf1cc
Alex Bartley Nees <alexbartleynees@gmail.com>
2025-09-22 19:41:02 +1200

    Update kanshi

--- home/hosts/desktop/desktop.nix
+++ home/hosts/desktop/desktop.nix
@@ -11,40 +11,45 @@
   services.kanshi = {
     enable = true;
 
-    profiles = {
-      coding = {
-        outputs = [
-          {
-            criteria = "DP-6";
-            mode = "2560x1440@165";
-            position = "0,0";
-            status = "enable";
-          }
-          {
-            criteria = "DP-4";
-            mode = "2560x1440@144";
-            position = "2560,0";
-            status = "enable";
-          }
-        ];
-      };
-
-      gaming = {
-        outputs = [
-          {
-            criteria = "DP-6";
-            mode = "2560x1440@165";
-            position = "0,0";
-            status = "enable";
-          }
-          {
-            criteria = "DP-4";
-            mode = "2560x1440@144";
-            position = "2560,0";
-            status = "disable";
-          }
-        ];
-      };
-    };
+    settings = [
+      {
+        profile = {
+          name = "coding";
+          outputs = [
+            {
+              criteria = "DP-6";
+              mode = "2560x1440@165";
+              position = "0,0";
+              status = "enable";
+            }
+            {
+              criteria = "DP-4";
+              mode = "2560x1440@144";
+              position = "2560,0";
+              status = "enable";
+            }
+          ];
+        };
+      }
+      {
+        profile = {
+          name = "gaming";
+          outputs = [
+            {
+              criteria = "DP-6";
+              mode = "2560x1440@165";
+              position = "0,0";
+              status = "enable";
+            }
+            {
+              criteria = "DP-4";
+              mode = "2560x1440@144";
+              position = "2560,0";
+              status = "disable";
+            }
+          ];
+        };
+      }
+    ];
   };
 }
