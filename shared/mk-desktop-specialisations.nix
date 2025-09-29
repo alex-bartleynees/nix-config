b85cb4696133655ab7f5a6906c5e1100157c31b1
Alex Bartley Nees <alexbartleynees@gmail.com>
2025-09-29 21:47:50 +1300

    Tidy up

--- shared/mk-desktop-specialisations.nix
+++ shared/mk-desktop-specialisations.nix
@@ -1,19 +1,20 @@
 { inputs, lib }:
-{ baseImports, theme, users, desktops ? [ ] }:
-let
-  mkDesktopSpecialisation = desktop:
-    let
-      shared =
-        import ./nixos-default.nix { inherit inputs theme users desktop lib; };
-      sharedImports = shared.getImports {
-        additionalImports = baseImports ++ [{ _module.args.theme = theme; }];
+({ baseImports, theme, users, desktops ? [ ] }:
+  let
+    mkDesktopSpecialisation = desktop:
+      let
+        shared = import ./nixos-default.nix {
+          inherit inputs theme users desktop lib;
+        };
+        sharedImports = shared.getImports {
+          additionalImports = baseImports ++ [{ _module.args.theme = theme; }];
+        };
+      in {
+        inheritParentConfig = false;
+        configuration = {
+          imports = sharedImports ++ [ ../core/desktops/${desktop}.nix ];
+        };
       };
-    in {
-      inheritParentConfig = false;
-      configuration = {
-        imports = sharedImports ++ [ ../core/desktops/${desktop}.nix ];
-      };
-    };
 
-in { specialisation = lib.genAttrs desktops mkDesktopSpecialisation; }
+  in { specialisation = lib.genAttrs desktops mkDesktopSpecialisation; })
 
--- shared/mk-linux-system.nix
+++ shared/mk-linux-system.nix
@@ -1,88 +1,88 @@
 { inputs }:
-{ hostPath, system ? "x86_64-linux", desktop ? "hyprland", users
-, themeName ? "tokyo-night", hostName, enableThemeSpecialisations ? false
-, enableDesktopSpecialisations ? false, desktopSpecialisations ? [ ]
-, additionalModules ? [ ], additionalSpecialArgs ? { }, }:
-let
-  inherit (inputs) nixpkgs;
+({ hostPath, system ? "x86_64-linux", desktop ? "hyprland", users
+  , themeName ? "tokyo-night", hostName, enableThemeSpecialisations ? false
+  , enableDesktopSpecialisations ? false, desktopSpecialisations ? [ ]
+  , additionalModules ? [ ], additionalSpecialArgs ? { }, }:
+  let
+    inherit (inputs) nixpkgs;
 
-  # Constants
-  allowUnfreeConfig = { allowUnfree = true; };
+    # Constants
+    allowUnfreeConfig = { allowUnfree = true; };
 
-  # Package sets
-  pkgs = import nixpkgs {
-    inherit system;
-    config = allowUnfreeConfig;
-  };
+    # Package sets
+    pkgs = import nixpkgs {
+      inherit system;
+      config = allowUnfreeConfig;
+    };
 
-  # Theme setup
-  theme = import ../core/themes/${themeName}.nix { inherit inputs pkgs; };
-  themes = import ../core/themes {
-    inherit inputs users;
-    lib = nixpkgs.lib;
-  };
-  themeSpecialisations = if enableThemeSpecialisations then
-    [
-      (themes.mkThemeSpecialisations {
-        baseImports = [ "${hostPath}/nixos/configuration.nix" ]
-          ++ (if builtins.pathExists "${hostPath}/modules" then
-            [ "${hostPath}/modules" ]
-          else
-            [ ]);
-        inherit desktop;
-      })
-    ]
-  else
-    [ ];
-
-  # Desktop specialisations
-  mkDesktopSpecialisations = import ./mk-desktop-specialisations.nix {
-    inherit inputs;
-    lib = nixpkgs.lib;
-  };
-  desktopSpecialisationModules = if enableDesktopSpecialisations then
-    [
-      (mkDesktopSpecialisations {
-        baseImports = [ "${hostPath}/nixos/configuration.nix" ]
-          ++ (if builtins.pathExists "${hostPath}/modules" then
-            [ "${hostPath}/modules" ]
-          else
-            [ ]);
-        inherit theme users;
-        desktops = desktopSpecialisations;
-      })
-    ]
-  else
-    [ ];
-
-  # Shared configuration
-  shared = import ../shared/nixos-default.nix {
-    inherit inputs theme desktop users;
-    lib = nixpkgs.lib;
-  };
-
-  # Host-specific modules
-  hostModules = [ "${hostPath}/nixos/configuration.nix" ]
-    ++ (if builtins.pathExists "${hostPath}/modules" then
-      [ "${hostPath}/modules" ]
+    # Theme setup
+    theme = import ../core/themes/${themeName}.nix { inherit inputs pkgs; };
+    themes = import ../core/themes {
+      inherit inputs users;
+      lib = nixpkgs.lib;
+    };
+    themeSpecialisations = if enableThemeSpecialisations then
+      [
+        (themes.mkThemeSpecialisations {
+          baseImports = [ "${hostPath}/nixos/configuration.nix" ]
+            ++ (if builtins.pathExists "${hostPath}/modules" then
+              [ "${hostPath}/modules" ]
+            else
+              [ ]);
+          inherit desktop;
+        })
+      ]
     else
-      [ ]) ++ (if desktop != "none"
-      && builtins.pathExists (../core/desktops + "/${desktop}.nix") then
-        [ ../core/desktops/${desktop}.nix ]
+      [ ];
+
+    # Desktop specialisations
+    mkDesktopSpecialisations = import ./mk-desktop-specialisations.nix {
+      inherit inputs;
+      lib = nixpkgs.lib;
+    };
+    desktopSpecialisationModules = if enableDesktopSpecialisations then
+      [
+        (mkDesktopSpecialisations {
+          baseImports = [ "${hostPath}/nixos/configuration.nix" ]
+            ++ (if builtins.pathExists "${hostPath}/modules" then
+              [ "${hostPath}/modules" ]
+            else
+              [ ]);
+          inherit theme users;
+          desktops = desktopSpecialisations;
+        })
+      ]
+    else
+      [ ];
+
+    # Shared configuration
+    shared = import ../shared/nixos-default.nix {
+      inherit inputs theme desktop users;
+      lib = nixpkgs.lib;
+    };
+
+    # Host-specific modules
+    hostModules = [ "${hostPath}/nixos/configuration.nix" ]
+      ++ (if builtins.pathExists "${hostPath}/modules" then
+        [ "${hostPath}/modules" ]
       else
-        [ ]);
+        [ ]) ++ (if desktop != "none"
+        && builtins.pathExists (../core/desktops + "/${desktop}.nix") then
+          [ ../core/desktops/${desktop}.nix ]
+        else
+          [ ]);
 
-in nixpkgs.lib.nixosSystem {
-  inherit system;
+  in nixpkgs.lib.nixosSystem {
+    inherit system;
 
-  specialArgs = {
-    inherit inputs users desktop hostName;
-  } // additionalSpecialArgs;
+    specialArgs = {
+      inherit inputs users desktop hostName;
+    } // additionalSpecialArgs;
 
-  modules = shared.getImports {
-    additionalImports = hostModules ++ additionalModules
-      ++ [{ _module.args.theme = theme; }] ++ themeSpecialisations
-      ++ desktopSpecialisationModules;
-  };
-}
+    modules = shared.getImports {
+      additionalImports = hostModules ++ additionalModules
+        ++ [{ _module.args.theme = theme; }] ++ themeSpecialisations
+        ++ desktopSpecialisationModules;
+    };
+  })
 
