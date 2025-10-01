a87d0159df70e267f39c24fc09a1d306034d6b3b
Alex Bartley Nees <alexbartleynees@gmail.com>
2025-10-01 20:58:38 +1300

    Fix formatting

--- hosts/macbook/default.nix
+++ hosts/macbook/default.nix
@@ -8,7 +8,12 @@ in nix-darwin.lib.darwinSystem {
     ./configuration.nix
     ../../users/alexbn.nix
     ../../shared/custom-options.nix
-    { _module.args.self = { self = inputs.self; inherit username homeDirectory }; }
+    {
+      _module.args.self = {
+        self = inputs.self;
+        inherit username homeDirectory;
+      };
+    }
   ] ++ (import ../../shared/darwin-home-manager.nix {
     inherit inputs username homeDirectory;
     extraModules = [ ../../home/home.nix ];
