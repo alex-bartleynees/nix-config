784cbbb221d6532d96bdbd65fa1407353bce0405
Alex Bartley Nees <alexbartleynees@gmail.com>
2025-10-03 21:35:57 +1300

    Update gaming workstation profile

--- core/profiles/gaming-workstation.nix
+++ core/profiles/gaming-workstation.nix
@@ -7,6 +7,9 @@ lib.mkIf config.profiles.gaming-workstation {
   gaming = {
     enable = true;
     streaming.enable = true;
+    streaming.gpu = "nvidia"; # GPU to use for game streaming
+    streaming.resolution = "2560x1440@164.96"; # Resolution for game streaming
+    streaming.monitor = 1; # Monitor to use for game streaming
   };
 
   # Nvidia GPU support
@@ -21,4 +24,10 @@ lib.mkIf config.profiles.gaming-workstation {
 
   # ZSA keyboard support
   voyager.enable = true;
+
+  # Disable wake-up for Logitech USB Receiver (C548)
+  services.udev.extraRules = ''
+    # Disable wake-up for Logitech USB Receiver (C548)
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="046d", ATTRS{idProduct}=="c548", ATTR{power/wakeup}="disabled"
+  '';
 }
--- hosts/desktop.nix
+++ hosts/desktop.nix
@@ -1,15 +1,1 @@
-{ ... }: {
-  profiles.gaming-workstation = true;
-
-  gaming = {
-    streaming.gpu = "nvidia"; # GPU to use for game streaming
-    streaming.resolution = "2560x1440@164.96"; # Resolution for game streaming
-    streaming.monitor = 1; # Monitor to use for game streaming
-  };
-
-  services.udev.extraRules = ''
-    # Disable wake-up for Logitech USB Receiver (C548)
-    SUBSYSTEM=="usb", ATTRS{idVendor}=="046d", ATTRS{idProduct}=="c548", ATTR{power/wakeup}="disabled"
-  '';
-
-}
+{ ... }: { profiles.gaming-workstation = true; }
