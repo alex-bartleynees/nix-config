4a0a9e0c64ecc5841c510fc4f1541e63bcffb29a
Alex Bartley Nees <alexbartleynees@gmail.com>
2025-10-15 17:14:47 +1300

    update work persistence

--- users/persistence/work-persistence.nix
+++ users/persistence/work-persistence.nix
@@ -1,3 +1,7 @@
 { username, ... }: {
-  workPersistPaths = [ "/home/${username}/.config/teams-for-linux" ];
+  workPersistPaths = [
+    "/home/${username}/.config/teams-for-linux"
+    "/home/${username}/.local-certs"
+    "/home/${username}/.local/share/mkcert"
+  ];
 }
