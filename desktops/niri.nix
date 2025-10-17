32b60ec0b0e023774301b2053c58fecb7b4bfe30
Alex Bartley Nees <alexbartleynees@gmail.com>
2025-10-18 08:35:40 +1300

    Fix niri screensharing

--- desktops/niri.nix
+++ desktops/niri.nix
@@ -18,14 +18,16 @@
 
   xdg.portal = {
     enable = true;
-    extraPortals =
-      [ pkgs.xdg-desktop-portal-gtk pkgs.xdg-desktop-portal-gnome ];
+    extraPortals = [
+      pkgs.xdg-desktop-portal-gtk
+      pkgs.xdg-desktop-portal-hyprland
+    ];
     config = {
       niri = {
-        default = [ "gnome" "gtk" ];
+        default = [ "hyprland" "gtk" ];
         "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
-        "org.freedesktop.impl.portal.ScreenCast" = [ "gnome" ];
-        "org.freedesktop.impl.portal.Screenshot" = [ "gnome" ];
+        "org.freedesktop.impl.portal.ScreenCast" = [ "hyprland" ];
+        "org.freedesktop.impl.portal.Screenshot" = [ "hyprland" ];
       };
     };
     xdgOpenUsePortal = true;
