0bb9271b219c3a0a656170d4e169a2b5ba3f493b
Alex Bartley Nees <alexbartleynees@gmail.com>
2025-10-18 07:54:09 +1300

    Update niri

--- desktops/niri.nix
+++ desktops/niri.nix
@@ -24,6 +24,8 @@
       niri = {
         default = [ "gnome" "gtk" ];
         "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
+        "org.freedesktop.impl.portal.ScreenCast" = [ "gnome" ];
+        "org.freedesktop.impl.portal.Screenshot" = [ "gnome" ];
       };
     };
     xdgOpenUsePortal = true;
