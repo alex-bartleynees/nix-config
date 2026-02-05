f6576829165773ed003b2c164e71c78194d022fc
Alex Bartley Nees <alexbartleynees@gmail.com>
2026-02-06 05:02:58 +1300

    Update kde.nix

--- desktops/kde.nix
+++ desktops/kde.nix
@@ -1,34 +1,40 @@
-{ pkgs, ... }: {
-  services.xserver = {
-    enable = true;
-    displayManager.sddm.enable = true;
-    desktopManager.plasma6.enable = true;
+{
+  nixosConfig = { pkgs, ... }: {
+    services.xserver = {
+      enable = true;
+      displayManager.sddm.enable = true;
+      desktopManager.plasma6.enable = true;
+    };
+
+    services.upower.enable = true;
+    services.accounts-daemon.enable = true;
+
+    programs.nm-applet.enable = true;
+
+    environment.systemPackages = with pkgs; [
+      polonium
+      kdePackages.kdeplasma-addons
+    ];
+
+    system.nixos.tags = [ "kde" ];
+
+    # XDG Portal configuration
+    xdg.portal = {
+      enable = true;
+      wlr.enable = true;
+      extraPortals = with pkgs; [ xdg-desktop-portal-kde ];
+    };
+
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      pulse.enable = true;
+      jack.enable = true;
+    };
   };
 
-  services.upower.enable = true;
-  services.accounts-daemon.enable = true;
-
-  programs.nm-applet.enable = true;
-
-  environment.systemPackages = with pkgs; [
-    polonium
-    kdePackages.kdeplasma-addons
-  ];
-
-  system.nixos.tags = [ "kde" ];
-
-  # XDG Portal configuration
-  xdg.portal = {
-    enable = true;
-    wlr.enable = true;
-    extraPortals = with pkgs; [ xdg-desktop-portal-kde ];
-  };
-
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    pulse.enable = true;
-    jack.enable = true;
-  };
-
+  homeConfig = { ... }:
+    {
+      # KDE manages its own configuration, no Home Manager config needed
+    };
 }
