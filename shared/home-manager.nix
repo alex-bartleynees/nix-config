e794499f6502da75b9d8d6c04edcb78041fe4c55
Alex Bartley Nees <alexbartleynees@gmail.com>
2026-02-08 11:35:08 +1300

    Add profile path validation in home-manager.nix helper

--- shared/home-manager.nix
+++ shared/home-manager.nix
@@ -18,8 +18,40 @@
 
     userProfiles = baseProfiles ++ additionalProfiles;
 
+    # Validate profiles exist and are home modules
+    validateProfile = profile:
+      let
+        profilePath = ../profiles/${profile}.nix;
+        profileExists = builtins.pathExists profilePath;
+        profileContent = if profileExists then builtins.readFile profilePath else "";
+        # Check if the file contains the homeModule marker in the first line
+        isHomeModule = profileExists && (lib.hasPrefix "# homeModule: true" profileContent);
+      in {
+        inherit profile profilePath profileExists isHomeModule;
+        error = if !profileExists then
+          "Profile '${profile}' does not exist at ${toString profilePath}"
+        else if !isHomeModule then
+          "Profile '${profile}' is not a home module (missing '# homeModule: true' marker at ${toString profilePath})"
+        else
+          null;
+      };
+
+    validatedProfiles = map validateProfile userProfiles;
+
+    invalidProfiles = lib.filter (p: p.error != null) validatedProfiles;
+
+    validationErrors = lib.concatMapStringsSep "\n" (p: "  - ${p.error}") invalidProfiles;
+
     profilePaths = map (profile: ../profiles/${profile}.nix) userProfiles;
   in {
+    assertions = [{
+      assertion = invalidProfiles == [ ];
+      message = ''
+        Home Manager profile validation failed for user '${username}':
+        ${validationErrors}
+      '';
+    }];
+
     home-manager = {
       extraSpecialArgs = {
         inherit inputs username homeDirectory theme desktop;
