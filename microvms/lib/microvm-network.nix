58e6d20d1f40f00113a7544f501b46f3cf30d564
Alex Bartley Nees <alexbartleynees@gmail.com>
2026-07-10 11:21:17 +1200

    Fix network helper

--- microvms/lib/microvm-network.nix
+++ microvms/lib/microvm-network.nix
@@ -4,7 +4,9 @@ lib.listToAttrs (lib.imap0 (i: name: {
   inherit name;
   value = {
     tapId = "vm-${name}";
-    mac = "02:00:00:00:00:${lib.toHexString (i + 1)}";
+    mac = "02:00:00:00:00:${
+        lib.fixedWidthString 2 "0" (lib.toHexString (i + 1))
+      }";
     ipAddress = "10.0.${toString i}.2";
     gateway = "10.0.${toString i}.1";
   };
